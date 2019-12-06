0539ec8da48cda62a626848e939e7886bf9f2e62
nldreyer <nldreyer@ksu.edu>
2019-12-06 14:33:29 -0600

    Completed in class work.

--- StringExtensions/Program.cs
+++ StringExtensions/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using ExtensionMethods;
 
 namespace StringExtensions
 {
@@ -9,8 +11,8 @@ namespace StringExtensions
         {
             List<string> writings = new List<string>()
             {
-                "Foo",
-                "Bar",
+                "foo",
+                "bar",
                 "A River Runs through It",
                 "The Five-Year Itch",
                 "The Bridges of Madison County",
@@ -22,12 +24,31 @@ namespace StringExtensions
             Console.WriteLine("Fun with string extensions!");
             Console.WriteLine("---------------------------");
 
-            foreach(string s in writings)
+            foreach (string s in writings)
             {
                 Console.WriteLine(s);
+                Console.WriteLine($"Word Count {s.WordCount()}");
                 Console.WriteLine("---------------------------");
             }
-            
+
+            foreach (string s in writings)
+            {
+                Console.WriteLine(s.Capitalize());
+                Console.WriteLine("---------------------------");
+            }
+
+            foreach (string s in writings)
+            {
+                Console.WriteLine(s.Decapitalize());
+                Console.WriteLine("---------------------------");
+            }
+
+            foreach (string s in writings)
+            {
+                Console.WriteLine(s.Titleize());
+                Console.WriteLine("---------------------------");
+            }
+
         }
     }
 }
---
+++ StringExtensions/StringExtensions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ExtensionMethods
+{
+    public static class StringExtensions
+    {
+        public static int WordCount(this String str)
+        {
+            return str.Split(new char[] { ' ', '.', '?', '\t', '\n' }, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+
+        public static string Capitalize(this String str)
+        {
+            string first = str[0].ToString().ToUpper();
+            return first + str.Substring(1);
+        }
+
+        public static string Decapitalize(this String str)
+        {
+            string first = str[0].ToString().ToLower();
+            return first + str.Substring(1);
+        }
+
+        public static string Titleize(this String title)
+        {
+            List<string> articles = new List<string>()
+            {
+                "a",
+                "an",
+                "the"
+            };
+            List<string> parts = new List<string>(title.Split(" "));
+            string first = parts[0];
+            if (articles.Contains(parts[0].ToLower()))
+            {
+                parts.RemoveAt(0);
+                parts.Add(", ");
+                parts.Add(first);
+                first = parts[0];
+                while (articles.Contains(first.ToLower()))
+                {
+                    parts.RemoveAt(0);
+                    parts.Add(" ");
+                    parts.Add(first);
+                    first = parts[0];
+                }
+            }
+            string result = "";
+            foreach (string part in parts)
+            {
+                result += part + " ";
+            }
+            return result;
+        }
+    }
+}
